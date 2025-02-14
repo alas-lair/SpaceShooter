07ab5ab64fbf397cb61c77bd3c261db66dfc9b30
Alastair <alas_lair@proton.me>
2025-02-13 21:25:06 -0600

    feat: add player movement code

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Processors;
 
 public class PlayerLogic : MonoBehaviour
 {
+    int speed = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate((verticalInput * Vector2.up * Time.deltaTime * speed) + (horizontalInput * Vector2.right * Time.deltaTime * speed));
     }
 }
