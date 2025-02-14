fc9b9ee69e4382e7de411a21d3a97260fc6ff0ba
Alastair <alas_lair@proton.me>
2025-02-13 22:37:06 -0600

    feat: add enemy spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    // Public variables
+    [SerializeField] GameObject enemy;
+    [SerializeField] float timeBetweenSpawns;
+
+    // Private variables
+    float elapsedSinceLastSpawn = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         
@@ -11,6 +18,11 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (elapsedSinceLastSpawn >= timeBetweenSpawns)
+        {
+            Instantiate(enemy, new Vector3(Random.Range(-8, 8), 5, 0), Quaternion.identity);
+            elapsedSinceLastSpawn = 0;
+        }
+        elapsedSinceLastSpawn += Time.deltaTime;
     }
 }
