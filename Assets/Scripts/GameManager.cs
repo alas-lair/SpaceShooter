413a09f1b2791101fb3612873f1d71688a031e6e
Alastair <alas_lair@proton.me>
2025-02-13 21:07:47 -0600

    feat: add GameManager functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,48 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    // Private variables
+    int score = 0;
+    // Public variables
+    [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text gameOverText;
+    [SerializeField] Button restartButton;
+
+    public int Score
+    {
+        get { return score; }
+        set
+        {
+            score = value;
+            scoreText.text = score.ToString();
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        score = 0;
+        gameOverText.enabled = false;
+        restartButton.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void GameOver()
+    {
+        gameOverText.enabled = true;
+        restartButton.enabled = false;
+    }
+
+    public void Restart()
+    {
+        Start();
     }
 }
