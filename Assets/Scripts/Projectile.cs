1a5b227b9000c5e3417c19dd1ccb72d4b0cf028b
Alastair <alas_lair@proton.me>
2025-02-13 22:04:11 -0600

    feat: add Projectile code

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    // Private variables
+    float elapsedTime = 0;
+
+    // Public variables
     [SerializeField] int speed;
+    [SerializeField] float dieAfterSeconds;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * Time.deltaTime * speed);
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= dieAfterSeconds)
+        {
+
+        }
     }
 }
